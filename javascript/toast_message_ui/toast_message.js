925f5658fce262d12118b215d494047632971915
thanhtam <jline.tamdt@gmail.com>
2022-10-13 11:42:10 +0700

    add img

--- javascript/toast_message_ui/toast_message.js
+++ javascript/toast_message_ui/toast_message.js
@@ -1,34 +1,69 @@
-const btnSuccess = document.getElementsByClassName('.btn__success');
-const btnError = document.getElementsByClassName('.btn__error');
-const btnInfo = document.getElementsByClassName('.btn__info');
+const btnSuccess = document.querySelector('.btn__success');
+const btnError = document.querySelector('.btn__error');
+const btnInfo = document.querySelector('.btn__info');
 
 function toast({ title = '', mess = '', type = 'info', duration = 3000 }) {
     const main = document.getElementById('toast');
+
     if (main) {
         const toast = document.createElement('div');
-        toast.classList.add('toast');
+        const icons = {
+            success: 'fa-circle-check',
+            error: 'fa-sharp fa-circle-xmark',
+            info: 'fa-circle-exclamation',
+        };
+        const icon = icons[type];
+        toast.classList.add('toast', `toast--${type}`);
+        const delay = (duration / 1000).toFixed(2);
+        toast.style.animation = `showMess ease 1s, fadeOut 1s ${delay}s forwards`;
+        const timeOut = setTimeout(() => {
+            main.removeChild(toast);
+        }, 6000);
+
+        toast.onclick = function (e) {
+            if (e.target.closest('.toast__close')) {
+                main.removeChild(toast);
+                clearTimeout(timeOut);
+            }
+        };
+
         toast.innerHTML = `
-        <div class="toast__icon">
-            <i class="fa-solid fa-circle-check"></i>
-        </div>
-        <div class="toast__body">
-            <p class="toast__ttl">Bạn đã đăng nhập thành công</p>
-            <p class="toast__desc">Chào mừng bạn đến với website Tâm</p>
-        </div>
-        <div class="toast__close">
-            <i class="fa-solid fa-xmark"></i>
-        </div>
+            <div class="toast__icon">
+                <i class="fa-solid ${icon}"></i>
+            </div>
+            <div class="toast__body">
+                <p class="toast__ttl">${title}</p>
+                <p class="toast__desc">${mess}</p>
+            </div>
+            <div class="toast__close">
+                <i class="fa-solid fa-xmark"></i>
+            </div>
         `;
         main.appendChild(toast);
     }
 }
-toast({
-    title: 'success',
-    mess: 'Bạn đã đăng nhập thành công',
-    type: 'success',
-    duration: 3000,
-});
 
-btnSuccess = onclick(function () {});
-btnError = onclick(function () {});
-btnInfo = onclick(function () {});
+btnSuccess.onclick = function () {
+    toast({
+        title: 'Bạn đã đăng nhập thành công',
+        mess: 'Chào mừng bạn đến với website Tâm',
+        type: 'success',
+        duration: 5000,
+    });
+};
+btnError.onclick = function () {
+    toast({
+        title: 'Đăng nhập thất bại',
+        mess: 'Tên đăng nhập hoặc mật khẩu không đúng',
+        type: 'error',
+        duration: 5000,
+    });
+};
+btnInfo.onclick = function () {
+    toast({
+        title: 'Đăng nhập thất bại',
+        mess: 'Lỗi hệ thống',
+        type: 'info',
+        duration: 5000,
+    });
+};
